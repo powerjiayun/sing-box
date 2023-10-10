7abb0f5b912b0f3397b8f4408970c03e5501d28d
yaotthaha <yaotthaha@foxmail.com>
2023-10-10 10:40:10 +0800

    Add reload support
    (cherry picked from commit https://github.com/rnetx/sing-box/commit/e98000c371095c1d5f7fcec4a02ffaaf22a68e47)

--- route/router.go
+++ route/router.go
@@ -49,9 +49,10 @@ type Router struct {
 	quicSniffCache sync.Map
 
 	defaultDomainMatchStrategy C.DomainMatchStrategy
+	reloadChan                 chan<- struct{}
 }
 
-func NewRouter(ctx context.Context, logFactory log.Factory, options option.RouteOptions, dnsOptions option.DNSOptions) *Router {
+func NewRouter(ctx context.Context, logFactory log.Factory, options option.RouteOptions, dnsOptions option.DNSOptions, reloadChan chan<- struct{}) *Router {
 	return &Router{
 		ctx:               ctx,
 		logger:            logFactory.NewLogger("router"),
@@ -68,6 +69,7 @@ func NewRouter(ctx context.Context, logFactory log.Factory, options option.Route
 		platformInterface: service.FromContext[adapter.PlatformInterface](ctx),
 
 		defaultDomainMatchStrategy: C.DomainMatchStrategy(options.DefaultDomainMatchStrategy),
+		reloadChan:                 reloadChan,
 	}
 }
 
@@ -274,4 +276,13 @@ func (r *Router) lookupQUICSniff(source, destination M.Socksaddr) (string, bool)
 
 func (r *Router) DefaultDomainMatchStrategy() C.DomainMatchStrategy {
 	return r.defaultDomainMatchStrategy
+}
+
+func (r *Router) Reload() {
+	if r.platformInterface == nil {
+		select {
+		case r.reloadChan <- struct{}{}:
+		default:
+		}
+	}
 }
